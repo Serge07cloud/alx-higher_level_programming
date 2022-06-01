93c60214350b05d9da7a0edb7a4f467d4a0e657e
Serge07cloud <wisdomandy.5@gmail.com>
2022-06-01 00:42:15 +0000

    Print integer

--- 0x00-python-hello_world/test.py
+++ 0x00-python-hello_world/test.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
-print(f"Best")
+# Ask the uer to input 2 values and store them in num1 and num2
+num1, num2 = input("Enter 2 numbers: ").split()
+# Convert the strings into regular numbers Integer
+num1 = int(num1)
+num2 = int(num2)
+
+# Add the value entered and store in sum
+sum = num1 + num2
+# Substract values and store them in difference
+defference = num1 - num2
+# Multiply the values and store them in product
+product = num1 * num2
+# Divide the values and store them in quotient
+quotient = num1 / num2
+# Use modulus on the values to find the remainder
+remainder = num1 % num2
+# Print the results
+print(f"{num1} + {num2} = {sum}")
+print(f"{num1} - {num2} = {defference}")
+print(f"{num1} * {num2} = {product}")
+print(f"{num1} / {num2} = {quotient}")
+print(f"{num1} % {num2} = {remainder}")
