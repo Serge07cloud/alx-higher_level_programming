7afd935c727a76bbe5edfda6b8a348ccae894e70
Serge07cloud <wisdomandy.5@gmail.com>
2022-06-04 17:54:58 +0000

    Print element of a list in reverse order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    list_copy = my_list.copy()
-    list_copy.reverse()
-    for i in range(len(list_copy)):
-        print('{:d}'.format(list_copy[i]))
+    my_list.reverse()
+    for i in my_list:
+        print('{:d}'.format(i))
