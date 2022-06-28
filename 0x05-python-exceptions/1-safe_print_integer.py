55cf424fabeae9834e805d00ef90b1c652450d8e
Serge07cloud <wisdomandy.5@gmail.com>
2022-06-28 12:38:33 +0000

    Print an integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# This function prints an integer value
+# Returns True if the value to print is an interger else print False
+def safe_print_integer(value):
+
+    try:
+
+        print("{:d}".format(value))
+
+        return True
+
+    except ValueError:
+
+        return False
